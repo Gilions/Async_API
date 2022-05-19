77a7e10a29e97b03727b39493990a22428f47b21
vitalijsofronuk <v.sofronyuk@puls.ru>
2022-05-19 17:06:06 +0300

    created basic folder structure

---
+++ app/config/urls.py
@@ -0,0 +1,16 @@
+from config import settings
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+
+if settings.DEBUG:
+    import mimetypes
+
+    import debug_toolbar
+
+    mimetypes.add_type("application/javascript", ".js", True)
+    urlpatterns += (path("__debug__/", include(debug_toolbar.urls)),)
